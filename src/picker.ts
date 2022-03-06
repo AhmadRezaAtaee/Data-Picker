0415895cd8dd37beb5d125857fb50397b7368574
AhmadRezaAtaee <ahmadr.ataee@gmail.com>
2022-03-06 20:45:49 +0330

    feat: add fetch method

--- src/picker.ts
+++ src/picker.ts
@@ -1,4 +1,6 @@
 // PickData - Picker - PickedData - PickData
+import axios, { AxiosResponse } from "axios";
+import { HttpRequestConfigDefault, HttpRequestConfig, HttpResponse } from "./http";
 import { Target } from "./target";
 
 export class Picker {
@@ -14,6 +16,43 @@ export class Picker {
 		if (typeof urlOrTarget === 'object') {
 			this.target = urlOrTarget
 		}
+	}
+
+	response: HttpResponse
+
+	fetch(): Promise<this>;
+	fetch(url: string, config?: Partial<Omit<HttpRequestConfig, 'url'>>): Promise<this>;
+	fetch(config: Partial<HttpRequestConfig>): Promise<this>;
+	async fetch(
+		urlOrConfig?: string | Partial<HttpRequestConfig>, config?: Partial<HttpRequestConfig>
+	) {
+
+		const RequestConfig: Partial<HttpRequestConfig> =
+			Object.assign(config || {}, HttpRequestConfigDefault)
+
+		let result: AxiosResponse;
+
+		switch (typeof urlOrConfig) {
+			case 'string':
+				result = await axios(urlOrConfig, RequestConfig)
+				break;
+
+			case 'object':
+				RequestConfig.url = RequestConfig.url || this.target.href
+				result = await axios(RequestConfig)
+				break;
+
+			default:
+				result = await axios(this.target.href, RequestConfig)
+				break;
+		}
+
+		this.target.setDoc(result.data)
+
+		const { config: _, statusText, request, ...response } = result
+		this.response = response
+
+		return this
 	}
 
 }
